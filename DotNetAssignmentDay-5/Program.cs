787887767ac8d8a82aefbe548ce7e6a9028c5225
srevanwar09 <srevanwar09@domain.com>
2022-07-20 20:51:00 +0530

    program are added

--- DotNetAssignmentDay-5/Program.cs
+++ DotNetAssignmentDay-5/Program.cs
@@ -1,4 +1,5 @@
-﻿
-double x1 = 2, x2 = 7, y1 = 5, y2 = 8;
-double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-Console.WriteLine("The length is : " + length);
+﻿int x1 = 2, x2 = 5, y1 = 9, y2 = 6;
+int x3 = 3, x4 = 8, y3 = 3, y4 = 7;
+double line1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+double line2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+Console.WriteLine(line1.Equals(line2));
