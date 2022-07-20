ab6a469ffa0e5ead231c82cd9bf186b9be8b3672
srevanwar09 <srevanwar09@domain.com>
2022-07-20 21:26:20 +0530

    program are added

--- DotNetAssignmentDay-5/Program.cs
+++ DotNetAssignmentDay-5/Program.cs
@@ -2,4 +2,4 @@
 int x3 = 3, x4 = 8, y3 = 3, y4 = 7;
 double line1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 double line2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
-Console.WriteLine(line1.Equals(line2));
+Console.WriteLine(line1.CompareTo(line2));
